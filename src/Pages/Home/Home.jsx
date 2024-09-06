3560052bdeb3f47c2038a5da69266f87419cde3b
MamunKhan2002 <mamunkhan2002bd@gmail.com>
2024-09-06 16:08:34 +0600

    My first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar/Navbar';
+import Footer from '../Shared/Footer/Footer';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            This is home...
+            <Footer />
+        </div>
+    );
+};
+
+export default Home;
